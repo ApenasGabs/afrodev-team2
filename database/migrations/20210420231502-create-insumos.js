94bfa66f5a528256a6ef1dbc0a3b9cff1dc70e6b
Gustavo Benedito <54513986+Gustavobenedito64a@users.noreply.github.com>
2021-04-20 20:44:24 -0300

    fixes #3 - create migration file

---
+++ database/migrations/20210420231502-create-insumos.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>queryInterface.createTable('inputs', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type:Sequelize.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    expiration_date: {
+      type: Sequelize.DATE,
+    },
+    quantity: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    unit: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: async (queryInterface, Sequelize) => {
+  }
+};
