9c458448a2a4267a9d11b2b2ccbdee8a0cf9c6d2
Arlene Pelenda <arlenej011@gmail.com>
2021-04-23 15:43:54 -0300

    Testes concluídos

--- __tests__/medicine.test.js
+++ __tests__/medicine.test.js
@@ -22,12 +22,13 @@ describe('Getting data from API - MEDICINE', () => {
         batch: "blabla"      
     });
     expect(res.status).toBe(200);
+    expect(res.body).not.toStrictEqual([])
   });
 
   it('Calling GET - all medicines', async () => {
     const res = await request.get('/medicines');
     expect(res.status).toBe(200);
-    //expect(res.body).toStrictEqual([]);
+    //expect(res.body).toBe([]);
   });
 
   it('Calling GET - one medicine', async () => {
@@ -44,18 +45,19 @@ describe('Getting data from API - MEDICINE', () => {
     const id = test.text.indexOf('id')
     const res = await request.get(`/medicines/${id}`);
     expect(res.status).toBe(200);
+    expect(res.status).not.toStrictEqual([])
   });
 
   it('PUT medicines', async() => {
     const test = await request.post('/medicines')
     .type('json')
     .send({
-        name: "rivotril",
-        manufacturer: "F.A",
-        prescription: false,
-        amount: 17,
-        expiration_date: "2022/09/18",
-        batch: "blabla"      
+      name: "rivotril",
+      manufacturer: "F.A",
+      prescription: false,
+      amount: 17,
+      expiration_date: "2022/09/18",
+      batch: "blabla"      
     });
     const id = test.text.indexOf('id')
     const res = await request.put(`/medicines/${id}`)
@@ -69,18 +71,19 @@ describe('Getting data from API - MEDICINE', () => {
       expiration_date: "2022/02/13"
     })
     expect(res.status).toBe(200)
+    expect(res.status).not.toStrictEqual([])
   });
 
   it('PATCH medicines', async () => {
     const test = await request.post('/medicines')
     .type('json')
     .send({
-        name: "rivotril",
-        manufacturer: "F.A",
-        prescription: false,
-        amount: 17,
-        expiration_date: "2022/09/18",
-        batch: "blabla"      
+      name: "rivotril",
+      manufacturer: "F.A",
+      prescription: false,
+      amount: 17,
+      expiration_date: "2022/09/18",
+      batch: "blabla"      
     });
     const id = test.text.indexOf('id')
     const res = await request.patch(`/medicines/${id}`)
@@ -89,22 +92,24 @@ describe('Getting data from API - MEDICINE', () => {
       amount: 13
     })
     expect(res.status).toBe(200)
+    expect(res.status).not.toStrictEqual([])
   });
 
   it('DELETE medicines', async () => {
     const test = await request.post('/medicines')
     .type('json')
     .send({
-        name: "rivotril",
-        manufacturer: "F.A",
-        prescription: false,
-        amount: 17,
-        expiration_date: "2022/09/18",
-        batch: "blabla"      
+      name: "rivotril",
+      manufacturer: "F.A",
+      prescription: false,
+      amount: 17,
+      expiration_date: "2022/09/18",
+      batch: "blabla"      
     });
     const id = test.text.indexOf('id')
     const res = await request.delete(`/medicines/${id}`)
     expect(res.status).toBe(200)
+    expect(res.status).not.toStrictEqual([])
   });
 
   afterAll(async (done) => {
