f1bec89825c1a39e971c1cbf95962f8bc6c6f94a
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2021-04-24 01:02:52 +0000

    fixed #3 - finalizing integration test

--- __tests__/inputs.test.js
+++ __tests__/inputs.test.js
@@ -19,6 +19,75 @@ describe('Getting data from API - Inputs', () => {
   });
 
   it('Should be able to create a new product', async () => {
+    const res = await request.post('/input')
+      .type('json')
+      .send({
+        name: 'Nescau',
+        type: 'Perecível',
+        expiration_date: '20/05/2022',
+        quantity: 5,
+        unit: 'KG',
+      });
+    expect(res.status).toBe(200);
+    expect(res.body).not.toStrictEqual([]);
+  });
+
+  it('Should be able to get one product', async () => {
+    const product = await request.post('/input').type('json').send({
+      name: 'Nescau',
+      type: 'Perecível',
+      expiration_date: '20/05/2022',
+      quantity: 5,
+      unit: 'KG',
+    });
+    const { id } = product.body;
+    const res = await request.get(`/input/${id}`);
+    expect(res.status).toBe(200);
+    expect(res.status).not.toStrictEqual([]);
+  });
+
+  it('Should be able to update all product information', async () => {
+    const test = await request.post('/input')
+      .type('json')
+      .send({
+        name: 'Nescau',
+        type: 'Perecível',
+        expiration_date: '20/05/2022',
+        quantity: 5,
+        unit: 'KG',
+      });
+    const { id } = test.body;
+    const res = await request.put(`/input/${id}`)
+      .send({
+        name: 'Abobora',
+        type: 'Perecível',
+        expiration_date: '30/04/2022',
+        quantity: 3,
+        unit: 'KG',
+      });
+    expect(res.status).toBe(200);
+    expect(res.status).not.toStrictEqual([]);
+  });
+
+  it('Should be able to update one information', async () => {
+    const product = await request.post('/input').type('json').send({
+      name: 'Nescau',
+      type: 'Perecível',
+      expiration_date: '20/05/2022',
+      quantity: 5,
+      unit: 'KG',
+    });
+    const { id } = product.body;
+    const res = await request.patch(`/input/${id}`)
+      .type('application/json')
+      .send({
+        quantity: 10,
+      });
+    expect(res.status).toBe(200);
+    expect(res.status).not.toStrictEqual([]);
+  });
+
+  it('Should be able to delete a product', async () => {
     const res = await request.post('/input').send({
       name: 'Nescau',
       type: 'Perecível',
@@ -26,7 +95,10 @@ describe('Getting data from API - Inputs', () => {
       quantity: 5,
       unit: 'KG',
     });
-    expect(res.status).toBe(200);
+    const { id } = res.body;
+    const del = await request.delete(`/input/${id}`);
+    expect(del.status).toBe(200);
+    expect(res.status).not.toStrictEqual([]);
   });
 
   afterAll(async (done) => {
